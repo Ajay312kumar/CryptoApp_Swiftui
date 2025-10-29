ca2a41bc20f40c11f45fbb7bd13cc2e8d4a7a178
Ajay <kumarraj312000@gmail.com>
2025-10-29 23:44:18 +0530

    created coin service model

---
+++ CrptoApp/Services/CoinDataService.swift
@@ -0,0 +1,51 @@
+//
+//  CoinDataService.swift
+//  CrptoApp
+//
+//  Created by Ajay kumar on 29/10/25.
+//
+
+import Foundation
+import Combine
+
+class CoinDataService {
+    
+    @Published var allCoins: [CoinModel] = []
+    var coinSubscription: AnyCancellable?
+    
+    init() {
+        getCoins()
+    }
+    
+    private func getCoins() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else {
+            return
+        }
+        
+        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { output -> Data in
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .finished:
+                        break
+                    case .failure(let error):
+                        print("Error fetching coins: \(error)")
+                    }
+                },
+                receiveValue: { [weak self] returnedCoins in
+                    self?.allCoins = returnedCoins
+                }
+            )
+    }
+}
+
+
